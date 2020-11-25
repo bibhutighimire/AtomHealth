df21ee815e00c336bb057e8dc63124d690e8fbf8
bibhutighimire <ghimirebibhuti@gmail.com>
2020-11-24 23:15:03 -0700

    Email to Visitor and Atom health both works with body text

--- AtomHealth/Controllers/ContactController.cs
+++ AtomHealth/Controllers/ContactController.cs
@@ -25,10 +25,10 @@ namespace AtomHealth.Controllers
                 try
                 {
                     MailMessage msz = new MailMessage();
-                    msz.From = new MailAddress(vm.Email);//Email which you are getting 
-                                                         //from contact us page 
+                    msz.From = new MailAddress(vm.Email, "Visitor Email");//Email which you are getting 
+                                                                          //from contact us page 
                     msz.To.Add("atomhealth1@gmail.com");//Where mail will be sent 
-                    msz.Subject = vm.Subject;
+                    msz.Subject = "ACTION REQUIRED: Visitor Email Requires Response";
 
 
                     msz.Body =
@@ -43,18 +43,18 @@ namespace AtomHealth.Controllers
                                 $"Message:{ Environment.NewLine}" +
                                  $"{vm.Message}";
 
-                    SmtpClient smtp = new SmtpClient();
+                    SmtpClient smtpc = new SmtpClient();
 
-                    smtp.Host = "smtp.gmail.com";
+                    smtpc.Host = "smtp.gmail.com";
 
-                    smtp.Port = 587;
+                    smtpc.Port = 587;
 
-                    smtp.Credentials = new System.Net.NetworkCredential
+                    smtpc.Credentials = new System.Net.NetworkCredential
                     ("atomhealth1@gmail.com", "Atomhealth@2020");
 
-                    smtp.EnableSsl = true;
+                    smtpc.EnableSsl = true;
 
-                    smtp.Send(msz);
+                    smtpc.Send(msz);
 
                     ModelState.Clear();
                     ViewBag.Message = "Thank you for Contacting us. We will get back to you soon.";
@@ -65,6 +65,32 @@ namespace AtomHealth.Controllers
                     ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
                 }
             }
+            //response to contact 
+
+            string to = vm.Email;
+            string subject = "Thanks for Contacting Us!";
+            string body =
+            $"Hello User!,{Environment.NewLine}" +
+            $"{ Environment.NewLine}" +
+                       $"We want to let you know that we received your email message. Someone from our office will get back to you within 2-4 days.{Environment.NewLine}" +
+                         $"{ Environment.NewLine}" +
+                               $"Once again, thanks for contacting us. Stay Safe!{Environment.NewLine}" +
+                               $"{ Environment.NewLine}" +
+                        $"ATOM HEALTH TEAM";
+
+            MailMessage mm = new MailMessage();
+            mm.To.Add(to);
+            mm.Subject = subject;
+            mm.Body = body;
+            mm.From = new MailAddress("atomhealth1@gmail.com", "Atom Health Team");
+            mm.IsBodyHtml = false;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = true;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("atomhealth1@gmail.com", "Atomhealth@2020");
+            smtp.Send(mm);
+
 
             return View();
         }
